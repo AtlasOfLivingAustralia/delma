a279e1c513d10fb1a6820690fe386c21bdbf9d1b
Martin Westgate <martinjwestgate@gmail.com>
2024-10-16 09:03:02 +1100

    re-order package-level help to place most useful content first

--- R/elm-package.R
+++ R/elm-package.R
@@ -12,31 +12,32 @@
 #' [support@ala.org.au](mailto:support@ala.org.au).
 #'
 #' @section Functions:
-#' **Example data**
-#'   * [metadata_example]
+#' **Top-level functions**
+#'   * [read_eml()] - read EML from an `xml` file to a `tibble`
+#'   * [read_md()] - read metadata from a markdown file to a `tibble`
+#'   * [write_eml()] - write an object to `xml`
+#'   * [write_md()] - write an object to markdown
 #'   * [add_eml_header()] - add an 'EML' header row to a `tibble`
-#'   * [use_metadata()]
-#'   * [check_eml()]
+#'   * [check_eml()] - run checks of a file or object against relevant standards
 #'   
-#' **Low-level functions**
-#'   * [parse_chr_to_tibble()]
-#'   * [parse_tibble_to_list()]
-#'   * [parse_list_to_xml()]
-#'   * [parse_xml_to_list()]
-#'   * [parse_list_to_tibble()]
-#'   * [parse_tibble_to_chr()]
-#' 
 #' **Intermediate-level functions**
 #'   * [as_eml_chr()] - convert to class `character`
 #'   * [as_eml_tibble()] - convert to class `tbl_df` (i.e. a tibble)
 #'   * [as_eml_list()] - convert to class `list`
 #'   * [as_eml_xml()] - convert to class `xml_document`
+#'   
+#' **Low-level functions**
+#'   * [parse_chr_to_tibble()] - convert from `character` to `tibble`
+#'   * [parse_tibble_to_list()] - convert from `tibble` to `list`
+#'   * [parse_list_to_xml()] - convert from `list` to `xml_document`
+#'   * [parse_xml_to_list()] - convert from `xml_document` to `list`
+#'   * [parse_list_to_tibble()] - convert from `list` to `tibble`
+#'   * [parse_tibble_to_chr()] - convert from `tibble` to `character`
+#' 
+#' **Example data**
+#'   * [metadata_example] - a boilerplate metadata statement
+#'   * [use_metadata()] - create a metadata statement as a markdown file
 #'
-#' **Top-level functions**
-#'   * [read_eml()] read EML from an `xml` file
-#'   * [read_md()] read a markdown file
-#'   * [write_eml()] write an object to `xml`
-#'   * [write_md()] write an object to markdown
 #'   
 #' @keywords internal
 "_PACKAGE"
